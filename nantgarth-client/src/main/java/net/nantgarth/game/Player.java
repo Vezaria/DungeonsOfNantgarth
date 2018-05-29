e659cca2af9b9d5f71fda4e6455317389d1193a6
Vezaria <Vezaria@users.noreply.github.com>
2018-05-29 14:14:31 +1000

    Fixed sticky collision

--- nantgarth-client/src/main/java/net/nantgarth/game/Player.java
+++ nantgarth-client/src/main/java/net/nantgarth/game/Player.java
@@ -7,6 +7,7 @@ import net.nantgarth.gfx.Animation;
 import net.nantgarth.gfx.Renderer;
 import net.nantgarth.input.Input;
 import net.nantgarth.math.Vector2f;
+import net.nantgarth.world.Level;
 import net.nantgarth.world.Wall;
 
 public class Player extends GameObject {
@@ -67,13 +68,25 @@ public class Player extends GameObject {
 		float bottom = newY;
 		float top = newY + 0.3f;
 		
-		boolean collision = false;
+		boolean collision = collision(left, right, position.y, position.y + 0.3f, g.level);
+		if(!collision) {
+			position.x = newX;
+		}
+		collision = collision(position.x + 0.2f, position.x + 0.8f, bottom, top, g.level);
+		if(!collision) {
+			position.y = newY;
+		}
 		
-		// TODO: Make a workaround for this label.
-		outer:
-		for(int y = 0; y < g.level.getHeight(); y++) {
-			for(int x = 0; x < g.level.getWidth(); x++) {
-				Wall w = g.level.wallAt(x, y);
+		if(Math.abs(velocity.x) < 0.000001f) velocity.x = 0;
+		if(Math.abs(velocity.y) < 0.000001f) velocity.y = 0;
+		
+		animation.update();
+	}
+	
+	public boolean collision(float left, float right, float bottom, float top, Level level) {
+		for(int y = 0; y < level.getHeight(); y++) {
+			for(int x = 0; x < level.getWidth(); x++) {
+				Wall w = level.wallAt(x, y);
 				if(w != null) {
 					float tLeft = x;
 					float tRight = x + 1f;
@@ -86,22 +99,12 @@ public class Player extends GameObject {
 					if(top > tBottom && bottom < tTop) yOverlap = true;
 					if(right > tLeft && left < tRight) xOverlap = true;				
 					
-					collision = xOverlap && yOverlap;
-					if(collision) {
-						break outer;
+					if(xOverlap && yOverlap) {
+						return true;
 					}
 				}
 			}
 		}
-		if(!collision) {
-			position.x = newX;
-			position.y = newY;			
-		}
-		
-		
-		if(Math.abs(velocity.x) < 0.000001f) velocity.x = 0;
-		if(Math.abs(velocity.y) < 0.000001f) velocity.y = 0;
-		
-		animation.update();
+		return false;
 	}
 }
